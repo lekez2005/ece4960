1b9a46cd2ca0c19385dcf5613c78c407a07f83c9
lekez2005 <lekez2005@yahoo.ca>
2018-04-01 14:48:25 -0400

    Compare normalized data with non-normalized

--- hacker_practice/03_21_18/least_squares.cpp
+++ hacker_practice/03_21_18/least_squares.cpp
@@ -9,6 +9,12 @@ namespace plt = matplotlibcpp;
 
 typedef Eigen::Matrix<double, 1, 1> Vector1d;
 
+//function to be optimized
+double (*opt_func)(const Eigen::VectorXd &params, const Eigen::VectorXd &x);
+// how 'least squares' is defined e.g. sum_square, lagrange
+double (*fx_func)(const Eigen::VectorXd &params, const Eigen::MatrixXd &data);
+// how gradient is calculated
+void (*grad_func)(const Eigen::VectorXd &params, const Eigen::MatrixXd &data, Eigen::VectorXd &grad);
 
 double norm(const Eigen::VectorXd& x){
     double res = 0;
@@ -17,27 +23,53 @@ double norm(const Eigen::VectorXd& x){
     return res;
 }
 
-// params[0] = a, params[1] = m
-double fx_power(const Eigen::Vector2d &params, const Vector1d &x){
-    return params[0]*std::pow(x[0], params[1]);
-}
-
-void gradient_pow(const Eigen::Vector2d &params, const Eigen::MatrixXd &data, Eigen::VectorXd &grad){
+void gradient_power(const Eigen::Vector2d &params, const Eigen::MatrixXd &data,
+        Eigen::VectorXd &grad, bool lagrange){
     grad[0] = 0;
     grad[1] = 0;
     for (int i = 0; i < data.rows(); i++){
         double x_pow_m = std::pow(data(i, 0), params[1]);
         double x_pow_prod = 2*x_pow_m*(params[0]*x_pow_m - data(i, 1));
+        if (std::fabs(data(i, 1)) > 1e-15 && lagrange) 
+            x_pow_prod /= std::pow(data(i, 1), 2);
         grad[0] += x_pow_prod;
         grad[1] += params[0]*std::log(data(i, 0))*x_pow_prod;
     }
 }
 
-double fx(const Eigen::VectorXd params, const Eigen::MatrixXd data){
+
+void grad_power_lagrange(const Eigen::VectorXd &params, const Eigen::MatrixXd &data,
+        Eigen::VectorXd &grad){
+    gradient_power(params, data, grad, true);
+}
+
+
+void grad_power_raw(const Eigen::VectorXd &params, const Eigen::MatrixXd &data,
+        Eigen::VectorXd &grad){
+    gradient_power(params, data, grad, false);
+}
+
+double fx_(const Eigen::VectorXd &params, const Eigen::MatrixXd &data, bool lagrange=false){
     double res = 0;
-    for (int i = 0; i < data.rows(); i++)
-        res += std::pow(fx_power(params, Vector1d(data(i, 0)))-data(i, 1), 2);
+    int rows = data.rows();
+    int cols = data.cols();
+    for (int i = 0; i < rows; i++){
+        double square_diff = std::pow(opt_func(params, data.block(i, 0, cols-1, 1))-data(i, 1),
+                    2);
+        if (std::fabs(data(i, 1)) > 1e-15 && lagrange)
+            square_diff /= std::pow(data(i, 1), 2);
+        res += square_diff;
+    }
     return res;
+}
+
+double fx_raw(const Eigen::VectorXd &params, const Eigen::MatrixXd &data){
+    return fx_(params, data, false);
+}
+
+
+double fx_lagrange(const Eigen::VectorXd &params, const Eigen::MatrixXd &data){
+    return fx_(params, data, true);
 }
 
 void hessian(const Eigen::VectorXd &params, const Eigen::VectorXd &grad, const Eigen::MatrixXd &data, 
@@ -48,7 +80,7 @@ void hessian(const Eigen::VectorXd &params, const Eigen::VectorXd &grad, const E
         Eigen::VectorXd tempGrad = grad;
         double delta = step*tempParams[i];
         tempParams[i] += delta;
-        gradient_pow(tempParams, data, tempGrad);
+        grad_func(tempParams, data, tempGrad);
         for (int j = 0; j < params.rows(); j++){
             hess(j, i) = (tempGrad[j]-grad[j])/delta;
         }
@@ -60,7 +92,7 @@ int fixed_t(Eigen::VectorXd &params, Eigen::VectorXd &delParams, const Eigen::Ma
         double *t, double curr_fx_val){
     int rows = params.rows();
     Eigen::VectorXd grad(rows);
-    gradient_pow(params, data, grad);
+    grad_func(params, data, grad);
     Eigen::MatrixXd hess(rows, rows);
     hessian(params, grad, data, hess);
 
@@ -74,7 +106,7 @@ int variable_t(Eigen::VectorXd &params, Eigen::VectorXd &delParams, const Eigen:
         double *t, double curr_fx_val){
     int rows = params.rows();
     Eigen::VectorXd grad(rows);
-    gradient_pow(params, data, grad);
+    grad_func(params, data, grad);
     Eigen::MatrixXd hess(rows, rows);
     hessian(params, grad, data, hess);
     
@@ -86,7 +118,7 @@ int variable_t(Eigen::VectorXd &params, Eigen::VectorXd &delParams, const Eigen:
         *t = std::pow(2, i);
         delParams = tempDelParams * *t;
         tempParams = params + delParams;
-        double temp_fx = fx(tempParams, data);
+        double temp_fx = fx_func(tempParams, data);
         if (std::fabs(temp_fx) <= std::fabs(curr_fx_val))
             break;
         i--;
@@ -107,7 +139,7 @@ void newton(const Eigen::VectorXd &params0, Eigen::VectorXd &soln, Eigen::Matrix
     Eigen::VectorXd params = params0;
     Eigen::VectorXd delParams;
 
-    double f_k = fx(params, data);
+    double f_k = fx_func(params, data);
     printf("%8s \t %8s \t %8s \t %8s \t %8s \t %8s \t %8s\n", "k", "a", "m",
             "norm(params)", "norm(delX)", "t", "f_x"); 
     printf("%8s \t %8g \t %8g \t %8g \t %8s \t %8s \t %8g\n", "0", params[0], params[1], norm(params), 
@@ -119,7 +151,7 @@ void newton(const Eigen::VectorXd &params0, Eigen::VectorXd &soln, Eigen::Matrix
         else
             status = variable_t(params, delParams, data, &t, f_k);
 
-        f_k = fx(params, data);
+        f_k = fx_func(params, data);
         printf("%8d \t %8g \t %8g \t %8g \t %8g \t %8g \t %8g \n", iterations, params[0], params[1],
                 norm(params), norm(delParams), t, f_k);
         iterations++;
@@ -132,7 +164,22 @@ void newton(const Eigen::VectorXd &params0, Eigen::VectorXd &soln, Eigen::Matrix
     soln = params;
 }
 
+void legend_loc(const std::string &location="best"){
 
+    PyObject* kwargs = PyDict_New();
+    PyDict_SetItemString(kwargs, "loc", PyString_FromString(location.c_str()));
+
+    PyObject* res = PyObject_Call(plt::detail::_interpreter::get().s_python_function_legend, plt::detail::_interpreter::get().s_python_empty_tuple, kwargs);
+    if(!res) throw std::runtime_error("Call to legend() failed.");
+
+    Py_DECREF(kwargs);
+    Py_DECREF(res);
+}
+
+// params[0] = a, params[1] = m
+double fx_power(const Eigen::VectorXd &params, const Eigen::VectorXd &x){
+    return params[0]*std::pow(x[0], params[1]);
+}
 
 int main(){
 
@@ -145,17 +192,23 @@ int main(){
 
 
 
-    Eigen::Vector2d params0(2.5, 2.25);
+    Eigen::Vector2d params0(2.6, 2);
     Eigen::VectorXd soln0(2);
     cout << "Initial condition a = " << params0[0] << " m = " << params0[1] << endl;
-    printf("\nQuasi-Newton With Line Search\n");
+    printf("\nQuasi-Newton With Line Search, Magnitude normalization\n");
+
+    opt_func = fx_power;
+    fx_func = fx_lagrange;
+    grad_func = grad_power_lagrange;
     newton(params0, soln0, data, false);
 
-
-    Eigen::Vector2d params1(2.8, 2.8);
+    fx_func = fx_raw;
+    grad_func = grad_power_raw;
+    //Eigen::Vector2d params1(2.8, 2.8);
     Eigen::VectorXd soln1(2);
-    cout << "Initial condition a = " << params1[0] << " m = " << params1[1] << endl;
-    newton(params1, soln1, data, false);
+    cout << "Initial condition a = " << params0[0] << " m = " << params0[1] << endl;
+    printf("\nQuasi-Newton With Line Search, No normalization\n");
+    newton(params0, soln1, data, false);
 
     Eigen::VectorXd x_vec = data.col(0);
     std::vector<double> x(x_vec.data(), x_vec.data() + x_vec.rows());
@@ -176,10 +229,11 @@ int main(){
         y1_plot[i] = soln1[0]*pow(x_point, soln1[1]);
     }
 
-    plt::plot(x, y, "*");
-    plt::named_plot("soln0", x_plot, y0_plot, "r");
-    plt::named_plot("soln1", x_plot, y1_plot, "b");
-    plt::legend();
+    plt::semilogy(x, y, "*");
+    plt::named_semilogy("Normalized Y", x_plot, y0_plot, "r");
+    plt::named_semilogy("Raw Y", x_plot, y1_plot, "b");
+    //plt::legend();
+    legend_loc("right");
     plt::save("fit_plot.png");
     //plt::show();
 
