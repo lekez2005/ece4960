10c2ab3e56d961918ecddbbd2fb5cdf4f810ef9d
lekez2005 <lekez2005@yahoo.ca>
2018-05-03 14:36:22 -0400

    Fix huen iterations bug

--- hacker_practice/03_29_18/huen_ode.cpp
+++ hacker_practice/03_29_18/huen_ode.cpp
@@ -25,9 +25,9 @@ int main() {
     double tol = 1e-7;
 
     double x_forward = 2;
-    double x_huen = 2;
+    double x_huen_one = 2;
     double x_huen_temp = 2;
-    double x_huen_tol = 2;
+    double x_huen_iter = 2;
 
     printf("%12s \t %12s \t %12s \t %12s \t %12s \t %12s \t %12s \t %12s \t %12s \n", "t", "x_true", "x_forward", "error", "x_huen_one", "error", 
             "x_huen", "iterations", "error");
@@ -35,23 +35,23 @@ int main() {
     for (int i = 0; i < num_steps; i++){
         x_forward += h*slope(t, x_forward);
 
-        x_huen_temp = x_huen + h*slope(t, x_huen);
-        x_huen += 0.5*h*(slope(t, x_huen) + slope(t+h, x_huen_temp));
+        x_huen_temp = x_huen_one + h*slope(t, x_huen_one);
+        x_huen_one += 0.5*h*(slope(t, x_huen_one) + slope(t+h, x_huen_temp));
 
-        double huen_old = x_huen_tol;
+        double x_huen_prev = x_huen_iter;
         int j;
         for (j = 0; j < max_steps; j++){
-            x_huen_temp = x_huen_tol + h*slope(t, x_huen_tol);
-            x_huen_tol = x_huen_tol + 0.5*h*(slope(t, x_huen_tol) + slope(t+h, x_huen_temp));
-            if (std::fabs(x_huen_tol-huen_old) < tol){
+            x_huen_temp = x_huen_iter + 0.5*h*(slope(t, x_huen_iter) + slope(t+h, x_huen_temp));
+            if (std::fabs(x_huen_temp-x_huen_prev) < tol){
                 break;
             }
-            huen_old = x_huen_tol;
+            x_huen_prev = x_huen_temp;
         }
+        x_huen_iter = x_huen_temp; 
         t = t + h;
         double x_real = 4/1.3*(std::exp(0.8*t)-std::exp(-0.5*t)) + 2*std::exp(-0.5*t);
         printf("%12g \t %12g \t %12g \t %12g \t %12g \t %12g \t %12g \t %12d \t %12g \n", t, x_real, x_forward, percent_err(x_forward, x_real), 
-                x_huen, percent_err(x_huen, x_real), x_huen_tol, j, percent_err(x_huen_tol, x_real));
+                x_huen_one, percent_err(x_huen_one, x_real), x_huen_iter, j, percent_err(x_huen_iter, x_real));
 
     }
 
